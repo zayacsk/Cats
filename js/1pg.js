4ee57dde5dc4ac183a12bd05510c8ae4406eac7b
Den <little1g0d@mail.ru>
2022-04-06 15:37:25 +0300

    added README.md

---
+++ js/1pg.js
@@ -0,0 +1,43 @@
+"use strict"
+
+
+function purpleFirst(){
+    document.getElementById("1_page").style.display = "none";
+    document.getElementById("2_page").style.display = "block";
+}
+function greenFirst(){
+    document.getElementById("1_page").style.display = "none";
+    document.getElementById("5_page").style.display = "block";
+}
+function purpleSecond(){
+    document.getElementById("2_page").style.display = "none";
+    document.getElementById("3_page").style.display = "block";
+}
+function greenSecond(){
+    document.getElementById("2_page").style.display = "none";
+    document.getElementById("6_page").style.display = "block";
+}
+function purpleThird(){
+    document.getElementById("3_page").style.display = "none";
+    document.getElementById("4_page").style.display = "block";
+}
+function greenThird(){
+    document.getElementById("3_page").style.display = "none";
+    document.getElementById("7_page").style.display = "block";
+}
+function pageFour(){
+    document.getElementById("4_page").style.display = "none";
+    document.getElementById("1_page").style.display = "block";
+}
+function pageFive(){
+    document.getElementById("5_page").style.display = "none";
+    document.getElementById("1_page").style.display = "block";
+}
+function pageSix(){
+    document.getElementById("6_page").style.display = "none";
+    document.getElementById("2_page").style.display = "block";
+}
+function pageSeven(){
+    document.getElementById("7_page").style.display = "none";
+    document.getElementById("1_page").style.display = "block";
+}
